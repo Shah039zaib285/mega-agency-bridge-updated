2038ac04bfea3842a41bd85eb6f89509638ea870
Shah Zaib <royalkhankundi@gmail.com>
2025-11-29 08:10:41 +0500

    Delete src directory

--- src/logger.js
+++
@@ -1,9 +0,0 @@
-import pino from "pino";
-import config from "./config.js";
-
-const logger = pino({
-  level: config.logLevel,
-  transport: { target: "pino-pretty" }
-});
-
-export default logger;
--- src/middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-import logger from "../logger.js";
-
-export function errorHandler(err, req, res, next) {
-  logger.error({ err, url: req.originalUrl }, "Unhandled error");
-  const status = err.status || 500;
-  res.status(status).json({ error: err.message || "Internal server error" });
-}
