8ed16c8f73c0c42b2b68613c0feaa873378e8aeb
Shah Zaib <royalkhankundi@gmail.com>
2025-11-29 07:59:40 +0500

    Update logger.js

--- src/logger.js
+++ src/logger.js
@@ -2,8 +2,8 @@ import pino from "pino";
 import config from "./config.js";
 
 const logger = pino({
-  level: config.logLevel || "info",
-  prettyPrint: process.env.NODE_ENV !== "production"
+  level: config.logLevel,
+  transport: { target: "pino-pretty" }
 });
 
 export default logger;
